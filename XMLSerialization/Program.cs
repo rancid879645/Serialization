0180c4d4018633f9181e9602c02839737f761003
Jonathan Velosa <jonathan_velosa@epam.com>
2023-12-22 17:07:17 -0500

    Xml serialization implemented
    *New clas XmlSerializationHelper was implemented with the methods to serialize and deserialize

--- XMLSerialization/Program.cs
+++ XMLSerialization/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using CommonLibrary;
+using XMLSerialization;
+
+Console.WriteLine("Department Name:");
+var departmentName = Console.ReadLine();
+Console.WriteLine("Employees Names separate by /:");
+var employees = Console.ReadLine()?.Split("/");
+Console.WriteLine("File path to serialize:");
+var filePath = Console.ReadLine();
+var department = new Department();
+
+if (employees != null && filePath != null)
+{
+    var employeesNames = employees.Select(employeeName => new Employee() { EmployeeName = employeeName }).ToList();
+    department.Employees = employeesNames;
+    department.DepartmentName = departmentName;
+    XmlSerializationHelper.Serialize(department, filePath);
+}
+
+Console.WriteLine("Deserialized Department:");
+
+if (filePath != null)
+{
+    var deserializedDepartment = XmlSerializationHelper.Deserialize<Department>(filePath);
+    var resultSb = new StringBuilder();
+    resultSb.Append($"Department Name: {deserializedDepartment.DepartmentName}\n");
+    foreach (var employee in deserializedDepartment.Employees)
+    {
+        resultSb.Append($"Employee Name: {employee.EmployeeName} \n");
+    }
+    Console.WriteLine(resultSb.ToString());
+}
+
+Console.WriteLine("Thanks for use our serializer :P");
