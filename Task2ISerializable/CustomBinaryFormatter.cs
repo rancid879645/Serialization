de596e95b9b5d9c939a959aab01e1fd334ae064c
Jonathan Velosa <jonathan_velosa@epam.com>
2023-12-26 12:50:36 -0500

    Task 2 implemented
    -new console application was created
    -the model Roles was created with hieracacly to Iserializable
    -New class implemented to serialize and deserialize

---
+++ Task2ISerializable/CustomBinaryFormatter.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.Serialization;
+using Task2ISerializable.Model;
+
+namespace Task2ISerializable
+{
+    public class CustomBinaryFormatter
+    {
+        public SerializationBinder Binder { get; set; }
+        public StreamingContext Context { get; set; }
+        public ISurrogateSelector SurrogateSelector { get; set; }
+
+        public object Deserialize(Stream serializationStream)
+        {
+            var reader = new BinaryReader(serializationStream);
+            var roleId = reader.ReadInt32();
+            var roleName = reader.ReadString();
+            return new Role{ RoleId = roleId, RoleName = roleName };
+        }
+
+        public void Serialize(Stream serializationStream, object graph)
+        {
+            var writer = new BinaryWriter(serializationStream);
+            var customObject = (Role)graph;
+            writer.Write(customObject.RoleId);
+            writer.Write(customObject.RoleName);
+        }
+    }
+}
---
+++ Task2ISerializable/Program.cs
@@ -0,0 +1,37 @@
+﻿using Task2ISerializable.Model;
+
+namespace Task2ISerializable;
+
+public class Program
+{
+    private static void Main()
+    {
+        var role = new Role()
+        {
+            RoleId = 1245,
+            RoleName = "Admin"
+        };
+        
+        SerializeWithCustomSerialization(role, "customSerialization.bin");
+        var deserializedObject = DeserializeWithCustomSerialization<Role>("customSerialization.bin");
+        
+        Console.WriteLine($"Deserialized Custom Object: RoleId - {deserializedObject.RoleId}, Property2 - {deserializedObject.RoleName}");
+    }
+
+    private static void SerializeWithCustomSerialization<T>(T obj, string filePath)
+    {
+        var formatter = new CustomBinaryFormatter();
+        using (var stream = new FileStream(filePath, FileMode.Create))
+        {
+            formatter.Serialize(stream, obj);
+        }
+        Console.WriteLine($"Custom Binary serialized and saved to {filePath}");
+    }
+
+    private static T DeserializeWithCustomSerialization<T>(string filePath)
+    {
+        var formatter = new CustomBinaryFormatter();
+        using var stream = new FileStream(filePath, FileMode.Open);
+        return (T)formatter.Deserialize(stream);
+    }
+}
